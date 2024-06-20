cd997c4ad652a9da7c1a30d6f95522ae1f809216
Mat Marquis <mat@matmarquis.com>
2024-06-20 13:16:33 -0400

    Tidying up.

--- functions/captions.js
+++ functions/captions.js
@@ -1,15 +1,15 @@
 'use strict'
 
-module.exports = function cap_plugin(md) {
-  var old = md.renderer.rules.image
-  md.renderer.rules.image = function (tokens, idx, options, env, self) {
-    if (tokens[idx].attrs[2]) {
-      let attrs = tokens[idx].attrs
-      let title = attrs[2][1]
-      let src = attrs[0][1]
-      let alt = tokens[idx].content
-      return `<figure><img src="${src}" alt="${alt}" title="${title}" /><figcaption>${title}</figcaption></figure>`
-    }
-    return old(tokens, idx, options, env, self)
-  }
-}
+module.exports = function cap_plugin( md ) {
+	md.renderer.rules.image = ( tokens, idx, options, env, self ) => {
+		if( tokens[idx].attrs[2] ) {
+			let attrs   = tokens[idx].attrs,
+				caption = attrs[2][1],
+				src     = attrs[0][1],
+				alt     = tokens[idx].content,
+				id      = 'id-' + ( performance.now().toString() ).replace(/\./gi, '');
+
+			return `<figure><img src="${ src }" alt="${ alt }" id="${ id}"><figcaption aria-describedby="${ id }">${ caption }</figcaption></figure>`;
+		}
+	}
+};
